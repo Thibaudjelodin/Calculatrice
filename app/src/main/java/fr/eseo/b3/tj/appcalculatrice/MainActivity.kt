975f111396792a326f91337195f548b5ea2981b8
Thibaudjelodin <151913080+thibaudjelodin@users.noreply.github.com>
2025-11-19 14:35:35 +0100

    Initial commit

---
+++ app/src/main/java/fr/eseo/b3/tj/appcalculatrice/MainActivity.kt
@@ -0,0 +1,47 @@
+package fr.eseo.b3.tj.appcalculatrice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fr.eseo.b3.tj.appcalculatrice.ui.theme.AppCalculatriceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppCalculatriceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppCalculatriceTheme {
+        Greeting("Android")
+    }
+}
